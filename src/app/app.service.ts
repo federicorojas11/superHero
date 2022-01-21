112b82261ab6cbd73ae5ec1f006ad9acfa32ae24
Federico Rojas <federico.rojas.121@gmail.com>
2022-01-21 16:01:04 -0300

    first commit

---
+++ src/app/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemorandumService {
+  endpoint = 'https://superheroapi.com/api/923777511838288';
+
+  constructor() {}
+}
